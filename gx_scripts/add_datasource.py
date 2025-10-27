93505da498681bcd0f13591aa942dff61580b336
Mekstp <suntiphap.5009@gmail.com>
2025-10-27 20:21:10 +0700

    Add files GX.

--- gx_scripts/add_datasource.py
+++ gx_scripts/add_datasource.py
@@ -41,8 +41,8 @@ datasource.add_table_asset(name="dim_customer", schema_name="mart_mart", table_n
 datasource.add_table_asset(name="dim_product", schema_name="mart_mart", table_name="dim_Product")
 datasource.add_table_asset(name="dim_location", schema_name="mart_mart", table_name="dim_Location")
 datasource.add_table_asset(name="dim_shipping", schema_name="mart_mart", table_name="dim_ShippingMode")
-datasource.add_table_asset(name="dim_date", schema_name="mart_mart", table_name="dim_OrderStatus")
-datasource.add_table_asset(name="dim_date", schema_name="mart_mart", table_name="dim_DeliveryStatus")
+datasource.add_table_asset(name="dim_orderstatus", schema_name="mart_mart", table_name="dim_OrderStatus")
+datasource.add_table_asset(name="dim_deliverystatus", schema_name="mart_mart", table_name="dim_DeliveryStatus")
 datasource.add_table_asset(name="dim_date", schema_name="mart_mart", table_name="dim_Date")
 
 
